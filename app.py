095e828a31ac731c6c4c4402cb392f3160c21dd9
hoabattu1991 <xedohiepsi@gmail.com>
2022-06-04 21:02:01 +0700

    Change text from home page

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = f"<h3>Sklearn Prediction Home</h3>"
+    html = f"<h1>Hello world. I'm a student from course Udacity</h1>"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
